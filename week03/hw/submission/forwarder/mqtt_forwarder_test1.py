858119235a706f3bbfda153e34fac1670c3f022d
Marcus A. Streips <mastreips@gmail.com>
2019-09-09 19:59:06 -0400

    Update mqtt_forwarder_test1.py

--- week03/hw/submission/forwarder/mqtt_forwarder_test1.py
+++ week03/hw/submission/forwarder/mqtt_forwarder_test1.py
@@ -1,42 +1,38 @@
 '''
 Sources:
 (1) http://www.steves-internet-guide.com/client-connections-python-mqtt/
+
+Author: Marcus A. Streips
+
+This forwarer script subscribes to the 'image' channel on a local MQTT broker and transmits ("forwards) the message to a 
+remote broker instance. It is essentially a relay.  This script was used as an alternative to setting up the local MQTT broker
+as a bridge to the remote broker instance.  The config file for the bridge is found in this directory for comparison purposes.
 '''
 
 import paho.mqtt.client as mqtt
 
-# keeping track of the IP of the MQTT brokers
+# MQTT network information
 local_broker_address = "172.18.0.2"
 remote_broker_address = "52.117.212.167"
 topic = "image"
 
-# subscribe to the topic on local mqtt connect
+# Function to create a local subscription service
 def on_connect_local(client, userdata, flags, rc):
-	print("connected to local mqtt")
+	print("connected to local broker")
 	client.subscribe(topic)
 
-# just showing it connected to the cloud mqtt
-def on_connect_remote(client, userdata, flags, rc):
-        print("connected to remote  mqtt")
-
-# when the local mqtt client receives new messages on the topic, publish to the cloud mqtt
+# Function to create a publishing service
 def on_message(client, userdata, msg):
-	#remote_client.on_publish = on_publish
 	remote_client.publish(topic, payload=msg.payload, qos=1, retain=False)
 
-#mqtt.Client.connected_flag = False
-
-# setting up local mqtt client and what it should do on_connect and on_message
-#local_client = mqtt.Client("forwarder_local")
+# Create instance of subscription service 
 local_client = mqtt.Client(client_id = "forwadrer_local", clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport='tcp')
 local_client.on_connect = on_connect_local
 local_client.connect(local_broker_address)
 local_client.on_message = on_message
 
-# setting up connection to the mqtt on the cloud
+# Create instance of publication service connecting to Cloud
 remote_client = mqtt.Client(client_id = "forwadrer_remote", clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport='tcp')
-#remote_client = mqtt.Client("forwarder_remote")  
-remote_client.on_connect = on_connect_remote
 remote_client.connect(remote_broker_address, 1883)
 
 local_client.loop_forever()
